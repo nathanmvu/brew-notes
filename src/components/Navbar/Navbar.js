ab80eb0c205918f2f9d9dd388fec1e23d6185bb1
Nathan Vu <nathanvu99@gmail.com>
2020-11-24 17:33:48 -0800

    Added components, pages, and some methods

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav class="navbar navbar-expand-md navbar-light">
+      <h1>
+        <a id='nameBadge' class="navbar-brand ml-5" href={(process.env.PUBLIC_URL || '/') + '/'}>BrewNotes</a>
+      </h1>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <Link to = {(process.env.PUBLIC_URL || '/') + '/'}>
+              <a class="nav-link" href='/'>Guides</a>
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to = {(process.env.PUBLIC_URL || '/') + '/notes'}>
+              <a class="nav-link" href='/'>Notes</a>
+            </Link>
+          </li>
+          <li class="nav-item">
+            <Link to = {(process.env.PUBLIC_URL || '/') + '/contact'}>
+              <a class="nav-link" href='/'>Sign Out</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/pages/Guides.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cards from './components/Cards/Cards';
+
+function Guides() {
+  return (
+    <div>
+      <Cards />
+    </div>
+  )
+}
+
+export default Guides;
