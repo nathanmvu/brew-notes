2530040c1f1f49262b9f97c4359cb53f9267b43b
Nathan Vu <nathanvu99@gmail.com>
2020-11-25 11:16:09 -0800

    Cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Switch>
+        <Route exact path={(process.env.PUBLIC_URL || '/') + '/'} component={About} />
+      </Switch>
+    </Router>
   );
 }
 
