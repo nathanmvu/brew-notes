be7c91986cbfd5211097dfa1eeb4a915322781a4
Nathan Vu <nathanvu99@gmail.com>
2020-12-01 10:22:57 -0800

    Moved app to client and added gitignore

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mongoose = require("mongoose");
+// const routes = require('./routes')
+const passport = require('passport');
+const session = require('express-session')
+const morgan = require('morgan');
+// const user = require('./routes/userApiRoutes');
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({ secret: 'keyboard cat' }));
+
+// Passport
+app.use(passport.initialize())
+app.use(passport.session()) // calls the deserializeUser
+
+// Define API routes here
+require('./server/utils/passport');
+require('./server/routes/userApiRoutes')(app, passport);
+require('./server/routes/noteApiRoutes')(app);
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/brewnotes");
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
